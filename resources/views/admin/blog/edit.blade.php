39cd634668449569ca367d117e3077e3791cde57
dwptra <muhamaddwiputranovriansyah@gmail.com>
2025-11-27 20:44:39 +0700

    edit blog

--- resources/views/admin/blog/edit.blade.php
+++ resources/views/admin/blog/edit.blade.php
@@ -1,7 +1,7 @@
-<form action="{{ route('admin.blog.update', $menu->id) }}" method="POST" enctype="multipart/form-data">
+<form action="{{ route('admin.blog.update', $blog->id) }}" method="POST" enctype="multipart/form-data">
     @csrf
 
-    <textarea name="description">{{ $menu->description }}</textarea>
+    <textarea name="description">{{ $blog->description }}</textarea>
     <input type="file" name="image">
 
     <button type="submit">Update</button>
