ca4ea85bf90c27c24f0b4a303901cf1817def4f3
shopia <shopiaamalia152@gmail.com>
2025-11-27 21:04:42 +0700

    style halaman blog(edit,tambah)

--- resources/views/admin/blog/create.blade.php
+++ resources/views/admin/blog/create.blade.php
@@ -1,8 +1,23 @@
-<form action="{{ route('admin.blog.store') }}" method="POST" enctype="multipart/form-data">
+<form action="{{ route('admin.blog.store') }}" 
+      method="POST" 
+      enctype="multipart/form-data"
+      class="create-form">
+
     @csrf
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+    <h2 class="create-form__title">Create Blog</h2>
 
-    <textarea name="content" placeholder="content"></textarea>
-    <input type="file" name="image">
+ 
+    <label class="create-form__label">
+        <span>Content</span>
+        <textarea name="content" placeholder="Write your blog content..."></textarea>
+    </label>
 
-    <button type="submit">Save</button>
+   
+    <label class="create-form__label create-form__file">
+        <span>Image</span>
+        <input type="file" name="image">
+    </label>
+
+    <button type="submit" class="create-form__btn">Save</button>
 </form>
--- resources/views/admin/blog/edit.blade.php
+++ resources/views/admin/blog/edit.blade.php
@@ -1,8 +1,22 @@
-<form action="{{ route('admin.blog.update', $blog->id) }}" method="POST" enctype="multipart/form-data">
+<form action="{{ route('admin.blog.update', $blog->id) }}" 
+      method="POST" enctype="multipart/form-data"
+      class="edit-form">
+
     @csrf
+    @method('PUT')
 
-    <textarea name="description">{{ $blog->description }}</textarea>
-    <input type="file" name="image">
+    <h2 class="edit-form__title">Edit Blog</h2>
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
 
-    <button type="submit">Update</button>
+    <label class="edit-form__label">
+        <span>Description</span>
+        <textarea name="description">{{ $blog->description }}</textarea>
+    </label>
+
+    <label class="edit-form__label edit-form__file">
+        <span>Image</span>
+        <input type="file" name="image">
+    </label>
+
+    <button type="submit" class="edit-form__btn">Update</button>
 </form>
