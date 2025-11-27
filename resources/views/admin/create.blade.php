f612d8ff7bc87d83afa5b6569375b7368a263f01
carissa <chanasaphira@gmail.com>
2025-11-27 20:39:41 +0700

    style tambah data admin

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
 
-<form action="{{ route('admin.menus.store') }}" method="POST" enctype="multipart/form-data">
+<form  class="create-form" action="{{ route('admin.menus.store') }}" method="POST" enctype="multipart/form-data">
 <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     @csrf
 
@@ -9,27 +9,27 @@
 <form action="{{ route('admin.menus.store') }}" method="POST" enctype="multipart/form-data">
 <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     @csrf
-    <label class="edit-form__label">
+    <label class="create-form__label">
         <span>Name</span>
         <input type="text" name="name">
     </label>
 
-    <label class="edit-form__label">
+    <label class="create-form__label">
         <span>Category</span>
         <input type="text" name="category">
     </label>
 
-    <label class="edit-form__label">
+    <label class="create-form__label">
         <span>Description</span>
         <textarea name="description"></textarea>
     </label>
 
-    <label class="edit-form__label">
+    <label class="create-form__label">
         <span>Harga</span>
         <input type="number" name="price">
     </label>
 
-    <label class="edit-form__label edit-form__file">
+    <label class="create-form__label create-form__file">
         <span>Gambar</span>
         <input id="imageInput" type="file" name="image">
         <div id="preview" class="edit-form__preview">
